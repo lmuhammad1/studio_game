3705fb44f4239ffbbeea0e067d9b2b93ce95e055
Louis Muhammad <louis.muhammad@gmail.com>
2014-08-01 14:48:41 -0400

    Finished Ch 10

--- studio_game.rb
+++ studio_game.rb
@@ -30,25 +30,46 @@ class Player
   end
 end
 
+class Game
+  def initialize(title)
+    @title = title
+    @players = []
+  end
 
-player1 = Player.new('moe')
-player2 = Player.new('larry', 60)
-player3 = Player.new('curly', 125)
-player4 = Player.new('shemp', 90)
-players = [player1, player2, player3]
-players.pop
-players.push(player4)
+  def add_player(a_player)
+    @players << a_player
+  end
+  attr_reader :title
 
-puts "There are #{players.count} players in the game."
-
-players.each do |player|
-  puts "#{player.name}'s health rating is #{player.health}."
+  def play
+    puts "There are #{@players.size} players in the #{@title}."
+    @players.each do |player|
+      puts player
+    end
+    @players.each do |player|
+      player.blam
+      player.w00t
+      player.w00t
+      puts player
+    end
+  end
 end
 
-players.each do |player|
-  player.blam
-  player.w00t
-  player.w00t
-  puts player
-end
+player1 = Player.new("moe")
+player2 = Player.new("larry", 60)
+player3 = Player.new("curly", 125)
+player4 = Player.new("alvin", 125)
+player5 = Player.new("simon", 125)
+player6 = Player.new("theodore", 125)
 
+knuckleheads = Game.new("Knuckleheads")
+knuckleheads.add_player(player1)
+knuckleheads.add_player(player2)
+knuckleheads.add_player(player3)
+knuckleheads.play
+
+chipmunks = Game.new("Chipmunks")
+chipmunks.add_player(player4)
+chipmunks.add_player(player5)
+chipmunks.add_player(player6)
+chipmunks.play
