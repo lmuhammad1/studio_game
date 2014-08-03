6864aba7554951f5e1258cbf79618fcc1a01e7aa
Louis Muhammad <louis.muhammad@gmail.com>
2014-08-03 16:47:59 -0400

    Finished Ch 17

--- game_spec.rb
+++ game_spec.rb
@@ -8,7 +8,7 @@ describe Game do
 
     @initial_health = 100
     @player = Player.new("moe", @initial_health)
-    
+
     @game.add_player(@player)
   end
 
@@ -35,6 +35,36 @@ describe Game do
 
     @player.health.should == @initial_health - (10 * 2)
   end
-  
+
+  it "assigns a treasure for points during a player's turn" do     
+    game = Game.new("Knuckleheads")
+    player = Player.new("moe")
+
+    game.add_player(player)
+
+    game.play(1)
+
+    player.points.should_not be_zero
+
+    # or use alternate expectation syntax:
+    # expect(player.points).not_to be_zero
+  end
+
+  it "computes total points as the sum of all player points" do
+    game = Game.new("Knuckleheads")
+
+    player1 = Player.new("moe")
+    player2 = Player.new("larry")
+
+    game.add_player(player1)
+    game.add_player(player2)
+
+    player1.found_treasure(Treasure.new(:hammer, 50))
+    player1.found_treasure(Treasure.new(:hammer, 50))
+    player2.found_treasure(Treasure.new(:crowbar, 400))
+
+    game.total_points.should == 500
+  end
+
 end
 
